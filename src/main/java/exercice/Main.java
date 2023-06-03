465dcfa78b872ebf55754902bd5328d1d7502f0c
flavieclr <flavie.carlier.66000@gmail.com>
2023-06-03 18:17:01 +0200

    Simplicite

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         String texte = "exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
-        System.out.println(Exercice.solution(texte,ordre));
+        System.out.println(Simplicite.solution(texte,ordre));
         // output: [classer, texte, exemple, a, de]
     }
 }
--- src/main/java/exercice/Simplicite.java
+++ src/main/java/exercice/Simplicite.java
@@ -1,11 +1,10 @@
 package exercice;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
-public class Exercice {
+public class Simplicite {
     public static List<String> solution(String str, List<Character> ordre) {
 
     String[] liste = str.split(" "); //creer un tableau de string dans chaque case un mot de la phrase (car on coupe apres le vide)
@@ -15,16 +14,7 @@ public class Exercice {
             mots.add(mot);
         }
 
-        Collections.sort(mots, new Comparator<String>() {
-           
-
-            public int compare(String mot1, String mot2) {
-                char c1 = mot1.charAt(0); //on redivise l'indice 0 apres chaque caractere 
-                char c2 = mot2.charAt(0);
-
-                return Integer.compare(ordre.indexOf(c1), ordre.indexOf(c2));
-            }
-        });
+        mots.sort(Comparator.comparingInt(a -> ordre.indexOf(a.charAt(0))));//on trie les mots en fonction de l'indice 0 de chaque mot
 
         return mots;
     }
