407d89bbc07778651d17a8ad36498af937bed2d0
flavieclr <flavie.carlier.66000@gmail.com>
2023-06-16 22:34:44 +0200

    SAEalgo

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -1,12 +1,13 @@
 package exercice;
 
+import java.time.Instant;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         String texte = "exemple de texte a classer";
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
-        System.out.println(Simplicite.solution(texte,ordre));
+        System.out.println(Exercice.solution(texte,ordre));
         // output: [classer, texte, exemple, a, de]
     }
 }
