c3e8606ab84144a7d6b135f74fbc4d5153945aec
flavieclr <flavie.carlier.66000@gmail.com>
2023-06-17 07:41:41 +0200

    saealgo

--- src/main/java/exercice/Main.java
+++ src/main/java/exercice/Main.java
@@ -9,5 +9,11 @@ public class Main {
         List<Character> ordre = List.of('c', 't', 'e', 'x', 'a', 'm', 'p', 'l', 's', 'r', 'd');
         System.out.println(Exercice.solution(texte,ordre));
         // output: [classer, texte, exemple, a, de]
+        
     }
+
+
+
+
+    
 }
