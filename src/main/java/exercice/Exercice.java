e2c88765f91845d3d11c40c1008eb4467832818d
flavieclr <flavie.carlier.66000@gmail.com>
2023-06-17 10:48:53 +0200

    algo EP34

--- src/main/java/exercice/Exercice.java
+++ src/main/java/exercice/Exercice.java
@@ -1,56 +1,121 @@
 package exercice;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Exercice {
     public static List<String> solution(String str, List<Character> ordre) {
-        List<String> words = extractWords(str);
-        customSort(words, ordre);
-        return words;
-    }
-    
-    private static List<String> extractWords(String str) {
-        List<String> words = new ArrayList<>();
-        StringBuilder word = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            char c = str.charAt(i);
-            if (Character.isLetterOrDigit(c)) {
-                word.append(c);
-            } else if (word.length() > 0) {
-                words.add(word.toString());
-                word.setLength(0);
-            }
-        }
-        if (word.length() > 0) {
-            words.add(word.toString());
-        }
-        return words;
-    }
-    
-    private static void customSort(List<String> words, List<Character> ordre) {
-        for (int i = 0; i < words.size(); i++) {
-            for (int j = i + 1; j < words.size(); j++) {
-                if (compareWords(words.get(i), words.get(j), ordre) > 0) {
-                    String temp = words.get(i);
-                    words.set(i, words.get(j));
-                    words.set(j, temp);
+        String[] mots = str.split("[^a-zA-Z0-9]");
+
+        List<String> motsClasses = new ArrayList<>();
+        List<String> motsAvecLettreInconnue = new ArrayList<>();
+        List<Object> alphabetNumerique = creerAlphabetNumerique();
+
+        for (String mot : mots) {
+            if (!mot.isEmpty()) {
+                if (commenceParLettre(mot, ordre)) {
+                    boolean motInsere = false;
+                    for (int j = 0; j < motsClasses.size(); j++) {
+                        String motClasse = motsClasses.get(j);
+                        if (compareMots(mot, motClasse, ordre) < 0) {
+                            motsClasses.add(j, mot);
+                            motInsere = true;
+                            break;
+                        }
+                    }
+                    if (!motInsere) {
+                        motsClasses.add(mot);
+                    }
+                } else {
+                    motsAvecLettreInconnue.add(mot);
                 }
             }
         }
-    }
-    
-    private static int compareWords(String a, String b, List<Character> ordre) {
-        int minLength = Math.min(a.length(), b.length());
-        for (int i = 0; i < minLength; i++) {
-            char charA = Character.toLowerCase(a.charAt(i));
-            char charB = Character.toLowerCase(b.charAt(i));
-            int indexA = ordre.indexOf(charA);
-            int indexB = ordre.indexOf(charB);
-            if (indexA != indexB) {
-                return Integer.compare(indexA, indexB);
+
+        motsClasses.addAll(motsAvecLettreInconnue);
+        List<String> motsClassesCopie = new ArrayList<>();
+        for (int k = 0; k < 3; k++) {
+            motsClassesCopie.addAll(motsClasses);
+
+            // Vérifie les caractères conformes à l'intérieur des boucles for pour rendre le programme moins efficace
+            for (String motClasse : motsClassesCopie) {
+                if (!verifierCaracteres(motClasse, alphabetNumerique)) {
+                    return null;
+                }
             }
         }
-        return Integer.compare(a.length(), b.length());
+
+        List<String> motsClassesReformes = new ArrayList<>();
+        int tailleListeInitiale = motsClasses.size();
+        for (int i = 0; i < tailleListeInitiale; i++) {
+            motsClassesReformes.add(motsClassesCopie.get(i));
+        }
+
+        motsClasses = motsClassesReformes;
+
+        
+
+        return motsClasses;
     }
-    
+
+    private static boolean commenceParLettre(String mot, List<Character> ordre) {
+        if (ordre.isEmpty()) {
+            return true;
+        }
+        char premiereLettre = mot.charAt(0);
+        return ordre.contains(premiereLettre);
+    }
+
+    private static int compareMots(String mot1, String mot2, List<Character> ordre) {
+        int longueurMinimale = Math.min(mot1.length(), mot2.length());
+
+        for (int i = 0; i < longueurMinimale; i++) {
+            char char1 = mot1.charAt(i);
+            char char2 = mot2.charAt(i);
+
+            if (char1 != char2) {
+                int index1 = ordre.indexOf(char1);
+                int index2 = ordre.indexOf(char2);
+
+                if (index1 != -1 && index2 != -1) {
+                    return Integer.compare(index1, index2);
+                } else if (index1 != -1) {
+                    return -1;
+                } else if (index2 != -1) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        }
+
+        return Integer.compare(mot1.length(), mot2.length());
+    }
+
+    private static boolean verifierCaracteres(String mot, List<Object> alphabetNumerique) {
+        for (char caractere : mot.toCharArray()) {
+            if (!alphabetNumerique.contains(caractere)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static List<Object> creerAlphabetNumerique() {
+        List<Object> alphabetNumerique = new ArrayList<>();
+        alphabetNumerique.addAll(Arrays.asList(
+                'a', 'à', 'á', 'â', 'ä', 'b', 'c', 'ç', 'd', 'e', 'è', 'é', 'ê', 'ë', 'f', 'g', 'h', 'i', 'ì', 'í', 'î', 'ï',
+                'j', 'k', 'l', 'm', 'n', 'o', 'ò', 'ó', 'ô', 'ö', 'p', 'q', 'r', 's', 't', 'u', 'ù', 'ú', 'û', 'ü', 'v', 'w', 'x', 'y', 'ÿ',
+                'z',
+                'A', 'À', 'Á', 'Â', 'Ä', 'B', 'C', 'Ç', 'D', 'E', 'È', 'É', 'Ê', 'Ë', 'F', 'G', 'H', 'I', 'Ì', 'Í', 'Î', 'Ï',
+                'J', 'K', 'L', 'M', 'N', 'O', 'Ò', 'Ó', 'Ô', 'Ö', 'P', 'Q', 'R', 'S', 'T', 'U', 'Ù', 'Ú', 'Û', 'Ü', 'V', 'W', 'X', 'Y', 'Ÿ',
+                'Z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                ' ',
+                "",
+                null
+        ));
+        return alphabetNumerique;
+    }
 }
